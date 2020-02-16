c3528990145b746845cb7f1d0991b56eb339c457
Thomas Persona <sc17lm@leeds.ac.uk>
2020-02-16 18:17:22 +0000

    Results page basic layout

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from app import app
 from flask import render_template, flash
 from .web_forms import MapURLForm
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = MapURLForm()
     return render_template('generate.html', form=form)
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    return render_template('result.html')
