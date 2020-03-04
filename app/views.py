2c7550584b2d8ef852fa35d5bd67f30757fe6933
Moore.l15 <sc17lm@leeds.ac.uk>
2020-03-04 00:48:30 +0000

    Ongoing work on scraper, currently trying to make work for all URL's also trying to ignore URLs out of the domain

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from app import app
 from flask import render_template, flash
 from .web_forms import MapURLForm
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError, HTTPError
 from bs4 import BeautifulSoup
 import re
+import time
 from anytree import Node, RenderTree
 
 
@@ -16,10 +17,7 @@ def index():
         delay = info[0]
         disallowed = info[1]
         allowed = info[2]
-        print("Delay: " + str(delay))
-        print("Disallowed size: " + str(len(disallowed)))
-        print("Allowed size: " + str(len(allowed)))
-        buildTree(("https://" + URL), delay, disallowed, allowed)
+        buildTree(("https://" + URL), delay, disallowed)
     return render_template('generate.html', form=form)
 
 
@@ -52,17 +50,60 @@ def getRobotTxt(URL):
                     elif "Crawl-delay: " in j:
                         delay = j[13:len(j)]
     if delay == 0:
-        delay = 3000
+        delay = 2000
     returnTuple = (delay, disallowed, allowed)
     return returnTuple
 
 
-def buildTree(URL, delay, disallowed, allowed):
+def buildTree(startUrl, delay, disallowed):
     scrapingQueue = []
-    htmlSoupObj = BeautifulSoup(urlopen(URL), 'html.parser')
-    title = htmlSoupObj.find_all('title')
-    for link in htmlSoupObj.find_all('a', attrs={'href': re.compile("^https://")}):
-        print(link.get('href'))
+    collectNodes(startUrl, scrapingQueue, disallowed, startUrl)
+    pagesScraped = 1
+    for link in scrapingQueue:
+        if pagesScraped == 100:
+            print("Finished Scraping, size of tree will consist of " + str(len(scrapingQueue)) + " nodes.")
+            break
+        time.sleep(int(delay)/1000)
+        collectNodes(str(link[1]), scrapingQueue, disallowed, startUrl)
+        pagesScraped += 1
+        print("Pages scraped: " + str(pagesScraped) + ", Size of scraping queue: " + str(len(scrapingQueue)))
+        duplicates = []
+        for elem in scrapingQueue:
+            if scrapingQueue.count(elem[1]) > 1:
+                duplicates.append(elem)
+    for i in scrapingQueue:
+        print i
+
+
+def collectNodes(url, scrapingQueue, disallowed, startUrl):
+    try:
+        print("here")
+        soupObj = BeautifulSoup(urlopen(url), features="html.parser")
+        title = soupObj.title.string
+    except (URLError, HTTPError, AttributeError) as e:
+        if e == URLError:
+            print("Error for link: " + url + "\n Error Reason: " + str(e.reason))
+        if e == HTTPError:
+            print("Error for link: " + url + "\n Error Reason: " + str(e.code))
+        if e == AttributeError:
+            print("No title for page")
+            title = "No Valid Title"
+        return
+    errorFound = False
+    for link in soupObj.findAll('a', attrs={'href': re.compile("^https://")}):
+        linkString = link.get('href')
+        i = 0
+        for elem in scrapingQueue:
+            if linkString in scrapingQueue[i][1]:
+                errorFound = True
+                break
+            i += 1
+        for na in disallowed:
+            if na in linkString:
+                errorFound = True
+        if not errorFound:
+            scrapingQueue.append(tuple((title, linkString)))
+    print(errorFound)
 
 
 def startLoop(list, index):
