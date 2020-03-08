ea369e6743e8df122fe3379d997e8fc022624437
Moore.l15 <sc17lm@leeds.ac.uk>
2020-03-08 19:59:32 +0000

    Remodelled the scraping functions, performing well and finding lots of links storing them appropriately to be used later in the tree assembly function. Need to think about how many pages are scraped and how it is displayed as a test of 100 pages yielded 181447 results.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from app import app
 from flask import render_template, flash
 from .web_forms import MapURLForm
-from urllib2 import urlopen, URLError, HTTPError
+from urllib2 import urlopen
 from bs4 import BeautifulSoup
-import re
+import requests
+import requests.exceptions
+from urlparse import urlparse, urlsplit
 import time
 from anytree import Node, RenderTree
 
@@ -33,22 +35,25 @@ def getRobotTxt(URL):
     delay = 0
     length = len(URL)
     robots = 'https://' + URL[4:length] + '/robots.txt'
-    contents = urlopen(robots)
-    for i in contents:
-        lines.append(i)
-    for i in lines:
-        if "User-agent: *" in i:
-            index = lines.index(i)
-            for j in startLoop(lines, (index + 1)):
-                if "User-agent: " in j:
-                    break
-                elif not isLineEmpty(j):
-                    if "Disallow: " in j:
-                        disallowed.append(j[10:len(j)])
-                    elif "Allow: " in j:
-                        allowed.append(j[7:len(j)])
-                    elif "Crawl-delay: " in j:
-                        delay = j[13:len(j)]
+    try:
+        contents = urlopen(robots)
+        for i in contents:
+            lines.append(i)
+        for i in lines:
+            if "User-agent: *" in i:
+                index = lines.index(i)
+                for j in startLoop(lines, (index + 1)):
+                    if "User-agent: " in j:
+                        break
+                    elif not isLineEmpty(j):
+                        if "Disallow: " in j:
+                            disallowed.append(j[10:len(j)])
+                        elif "Allow: " in j:
+                            allowed.append(j[7:len(j)])
+                        elif "Crawl-delay: " in j:
+                            delay = j[13:len(j)]
+    except Exception:
+        print("Error encountered: Unable to establish link to robot.txt file resorting to default values")
     if delay == 0:
         delay = 2000
     returnTuple = (delay, disallowed, allowed)
@@ -57,53 +62,80 @@ def getRobotTxt(URL):
 
 def buildTree(startUrl, delay, disallowed):
     scrapingQueue = []
-    collectNodes(startUrl, scrapingQueue, disallowed, startUrl)
-    pagesScraped = 1
-    for link in scrapingQueue:
-        if pagesScraped == 100:
-            print("Finished Scraping, size of tree will consist of " + str(len(scrapingQueue)) + " nodes.")
+    scraped = []
+    local = []
+    otherSites = []
+    pages = 0
+    i = 0
+    collectNodes(startUrl, scrapingQueue, local, otherSites)
+    while len(scrapingQueue):
+        urlAlreadyScraped = False
+        if pages == 100:
             break
-        time.sleep(int(delay)/1000)
-        collectNodes(str(link[1]), scrapingQueue, disallowed, startUrl)
-        pagesScraped += 1
-        print("Pages scraped: " + str(pagesScraped) + ", Size of scraping queue: " + str(len(scrapingQueue)))
-        duplicates = []
-        for elem in scrapingQueue:
-            if scrapingQueue.count(elem[1]) > 1:
-                duplicates.append(elem)
-    for i in scrapingQueue:
-        print i
-
-
-def collectNodes(url, scrapingQueue, disallowed, startUrl):
-    try:
-        print("here")
-        soupObj = BeautifulSoup(urlopen(url), features="html.parser")
-        title = soupObj.title.string
-    except (URLError, HTTPError, AttributeError) as e:
-        if e == URLError:
-            print("Error for link: " + url + "\n Error Reason: " + str(e.reason))
-        if e == HTTPError:
-            print("Error for link: " + url + "\n Error Reason: " + str(e.code))
-        if e == AttributeError:
-            print("No title for page")
-            title = "No Valid Title"
-        return
-    errorFound = False
-    for link in soupObj.findAll('a', attrs={'href': re.compile("^https://")}):
-        linkString = link.get('href')
-        i = 0
-        for elem in scrapingQueue:
-            if linkString in scrapingQueue[i][1]:
-                errorFound = True
+        url = scrapingQueue[i][1]
+        for page in scraped:
+            if url == page:
+                print "here"
+                urlAlreadyScraped = True
                 break
-            i += 1
-        for na in disallowed:
-            if na in linkString:
-                errorFound = True
-        if not errorFound:
-            scrapingQueue.append(tuple((title, linkString)))
-    print(errorFound)
+        if not urlAlreadyScraped:
+            if not checkUrlBanned(url, disallowed):
+                time.sleep(delay/1000)
+                collectNodes(url, scrapingQueue, local, otherSites)
+                scraped.append(url)
+                pages += 1
+        i += 1
+    print "Results after 100 pages scraped:\n\n"
+    print str(len(scrapingQueue))
+
+
+def collectNodes(url, scrapingQueue, local, otherSites):
+    try:
+        resp = requests.get(url)
+    except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL,
+           requests.exceptions.InvalidSchema):
+        print "Error scraping: " + str(url)
+        return
+
+    linksplit = urlsplit(url)
+    root = "{0.netloc}".format(linksplit)
+    root = root.replace("www.", "")
+    siteRoot = "{0.scheme}://{0.netloc}".format(linksplit)
+    path = url[:url.rfind('/')+1] if '/' in linksplit.path else url
+
+    soupObj = BeautifulSoup(resp.text, "html.parser")
+    newLink = ""
+    try:
+        parent = soupObj.title.string
+    except(AttributeError):
+        parent = "No Parent Title Found"
+    print("Pages found: " + str(len(soupObj.findAll('a'))))
+    for link in soupObj.findAll('a'):
+        a = link.attrs["href"] if "href" in link.attrs else ''
+        if a.startswith('/'):
+            newLink = siteRoot + a
+            local.append(newLink)
+        elif root in a:
+            local.append(a)
+        elif not a.startswith('http'):
+            newLink = path + a
+            local.append(newLink)
+        else:
+            otherSites.append(a)
+
+    for i in local:
+        scrapingQueue.append(tuple((parent, i)))
+
+
+def checkUrlBanned(url, disallowed):
+    for i in disallowed:
+        if "*" in i:
+            path = i.replace('*', '')
+            if path in url:
+                return True
+        elif i in url:
+            return True
+    return False
 
 
 def startLoop(list, index):
